f4fa25ad72eea68a7e45d7f4b211458e33409464
Sahil002620Q <smax06388@gmail.com>
2026-02-28 20:33:04 +0530

    port scanner

---
+++ Port-scanner/loading.py
@@ -0,0 +1,10 @@
+import sys
+import time
+
+for i in range(30):
+    dots = "." * (i % 4)
+    sys.stdout.write("\rSearching" + dots + "   ")
+    sys.stdout.flush()
+    time.sleep(0.3)
+
+print("\nDone!")
