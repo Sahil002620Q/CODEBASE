e0e5b840e377d725625a2bd6b2af177b988fc1c8
Sahil002620Q <smax06388@gmail.com>
2026-03-03 20:54:50 +0530

    python test

--- py-practice-codes/test26.py
+++ py-practice-codes/test26.py
@@ -4,12 +4,12 @@ list = input().split()
 print(list)
 print(min(list))
 import time
-lists = ['hello',
+lists = ['hello'
         ,'myself sahil']
-list = input().space()
+list = input().split()
 for line in list:
   for ch in line:
     print(ch,end='',flush=True)
-          time.sleep(0.1)
+    time.sleep(0.1)
   print()
-# code for lien animation 
+# code for line animation 
