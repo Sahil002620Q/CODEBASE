aa9be897618d3ef732e77867d51e84da8abc8591
Sahil002620Q <smax06388@gmail.com>
2026-03-13 20:54:33 +0530

    way to random pass generator

---
+++ wait.py
@@ -0,0 +1,12 @@
+import random
+
+x = ['a','b','c']
+n =[1,2,3]
+u =random.randint(1,99)
+v = random.choice(x)
+c = random.choice(n)
+
+k = random.choices(x) and random.choice(n) and random.choice(n) and random.choice(x)
+# if k == x:
+#     bc = random.choices
+print(k)
