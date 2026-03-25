d3a72573b02d007dc0c24172a8789fdd69dc9046
Sahil002620Q <smax06388@gmail.com>
2026-03-25 21:25:11 +0530

    js -l

---
+++ rex.js
@@ -0,0 +1,56 @@
+{
+  Trading {
+    Tokens(
+      limit: {count: 10}
+      orderBy: {descendingByField: "change"}
+      where: {Volume: {Usd: {gt: 100000}}, Interval: {Time: {Duration: {eq: 60}}}}
+    ) {
+      Token {
+        Address
+        Did
+        Id
+        IsNative
+        Name
+        Network
+        Name
+        Symbol
+        TokenId
+      }
+      Currency {
+        Symbol
+        Id
+        Name
+      }
+      Interval {
+        VolumeBased
+        Time {
+          Start
+          End
+        }
+      }
+      Volume {
+        Base
+        Quote
+        Usd
+      }
+      Price {
+        IsQuotedInUsd
+        Ohlc {
+          Close
+          High
+          Low
+          Open
+        }
+        Average {
+          Estimate
+          ExponentialMoving
+          Mean
+          SimpleMoving
+          WeightedSimpleMoving
+        }
+      }
+      diff: calculate(expression: "Price_Ohlc_Close - Price_Ohlc_Open")
+      change: calculate(expression: "round(($diff / Price_Ohlc_Open), 3) * 100")
+    }
+  }
+}
