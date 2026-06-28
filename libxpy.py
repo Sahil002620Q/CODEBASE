f5a5afd5aea1801d504957d6337b6ceeb0f4dce9
Sahil002620Q <smax06388@gmail.com>
2026-06-28 19:27:02 +0530

    creaded line anime function which will print alphabet in line with time interval and after printing one line it will print new line but as here i used print slash n it added one more new line as by default after execution it moves to next line so i used end to specify that after going to new line stay at that line and prevent default go to next line

--- libxpy.py
+++ libxpy.py
@@ -8,3 +8,17 @@ def search(loop,delay):
     print("\r            \r",end='')
 search(1,0.5)
 
+def lineanime():
+    import time
+    list = [
+        "hello sahil",
+        "how are you"
+        ]
+    
+    for line in list:
+        for alphabet in line:
+            print(alphabet,end='')
+            time.sleep(0.2)
+        print("\n",end='')   #using end=""  because by default it is print("\n", end="\n")
+
+lineanime()
