56f32281053d1f6f07df2768e633bfcddb95a627
Sahil002620Q <smax06388@gmail.com>
2026-07-01 12:25:58 +0530

    learned name main which is usefull if someone imports your file but special condition or code which is for testing or your file will work on your file only --source visually explained

---
+++ OOP/namemain.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    c = a + b
+    return c 
+
+
+if __name__ == "__main__":
+    if add(1,99) == 100:
+        print(True)
+    # else:
+    #     print(False)
+
+
+add(1,99)
+add(1,20)
+
---
+++ OOP/namemain2.py
@@ -0,0 +1,4 @@
+import namemain
+
+namemain.add(1,100)
+namemain.add(2,100)
