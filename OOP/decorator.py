f5e28f33f6eaa5c4f1d25d47e5c3387918b86b76
Sahil002620Q <smax06388@gmail.com>
2026-07-01 12:23:04 +0530

    learned decorators and args kwargs --args kwargs source visually explained

--- OOP/decorator.py
+++ OOP/decorator.py
@@ -1,6 +1,6 @@
 def greet(fx):
     def zfx(*args,**kwargs):  #it will run without arg kwarg but will not take input in add(x,y) and throw error  whe add() is runned 
-        print("start")
+        print("start")        # *arg takes all extra arguments and stores it in tupple name args while **kwargs takes all extra dict values and stores it in kwargs variable ,  * create empty tuple and store extra arguments while ** takes all arges with key and value and stores it in dict 
         fx(*args,**kwargs)
         print("end\n")
     return zfx
@@ -24,3 +24,5 @@ hello()
 add(1,3)
 add(3,300)
 greet(sub)(100,1)
+
+#need to be learned deeply
--- python/py-problems/aa.py
+++ python/py-problems/aa.py
@@ -1,8 +1,10 @@
 import sys
 import time
-for i in range(20):
-    dots = "."*(i%3)
-    sys.stdout.write("\rSearching" + dots + "   ")
-    time.sleep(0.1)
-    sys.stdout.flush()
-    time.sleep(0.2)
+def search():
+    for i in range(20):
+        dots = "."*(i%3)
+        sys.stdout.write("\rSearching" + dots + "   ")
+        time.sleep(0.1)
+        sys.stdout.flush()
+        time.sleep(0.2)
+search()
