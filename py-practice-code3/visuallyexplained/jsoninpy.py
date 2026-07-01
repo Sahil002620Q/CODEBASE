ab786038c479df2f06a1b43dd6696ccd7d2356b4
Sahil002620Q <smax06388@gmail.com>
2026-07-01 14:28:05 +0530

    file handling in python by --visually explained

---
+++ py-practice-code3/visuallyexplained/jsoninpy.py
@@ -0,0 +1,11 @@
+# file = open("firstcode.json","r")
+# content = file.read()
+# print(content)
+# file.close()
+
+file = open("firstcode.json","w")
+x = {"who":"me"}
+content = file.write(x)
+file.add()
+print(content)
+file.close()
