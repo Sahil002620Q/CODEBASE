56ed5f3ae84a8fa2a7947da6156bacab5497a7a9
Sahil002620Q <smax06388@gmail.com>
2026-07-03 01:44:09 +0530

    updated functions to take arguments for lineanime

--- py-practice-code3/lib.py
+++ py-practice-code3/lib.py
@@ -6,14 +6,11 @@ def search(loop,delay):
             print(f'\r{j}',end='   ')
             time.sleep(delay)
     print("\r            \r",end='')
-search(1,0.5)
 
-def lineanime():
+
+def lineanime(list):
     import time
-    list = [
-        "hello sahil",
-        "how are you"
-        ]
+    list 
     
     for line in list:
         for alphabet in line:
@@ -21,4 +18,35 @@ def lineanime():
             time.sleep(0.2)
         print("\n",end='')   #using end=""  because by default it is print("\n", end="\n")
 
-lineanime()
+
+
+def clear(inp):  #hidden bug like message
+    import sys
+    import time
+
+    print(inp)
+
+    time.sleep(0.2)
+    sys.stdout.write("\033[F")   # Move to previous line
+    sys.stdout.write("\033[2K")  # Clear entire line
+    sys.stdout.write("\r")   # Move to next line
+    sys.stdout.flush()
+    
+
+
+
+ascciv = [70,117,99,107,32,89,111,117]
+text = ''
+for i in ascciv:
+    text += chr(i)
+
+
+# print("\n",end='')
+
+
+search(1,0.5)
+clear(text)
+lineanime(["hello sahil","how are you"])
+
+#make a game which use random and file handeling it will read file containing word list using random it will replace some character with _ , user will fill the correct value , set lim like each work can use _ max 2 or 3 time # for difficult 2 easy 
+#make a repo which in which a file will take input and convert it into ascci list 
