973516f2351221805edfae506c0954bb670d533c
Sahil002620Q <smax06388@gmail.com>
2026-07-06 08:13:16 +0530

    all files included

--- c-practice-codes/countdown-using-while.c
+++ c-practice-codes/countdown-using-while.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <windows.h>
 int main () {
     int coutd = 10;
     while (coutd >= 0)
     {
         printf("%d\n",coutd);
         coutd--;
+        sleep(1)
     }
     printf("BOOM!\n");
 }
--- py-practice-code3/lib.py
+++ py-practice-code3/lib.py
@@ -14,7 +14,7 @@ def lineanime(list):
     
     for line in list:
         for alphabet in line:
-            print(alphabet,end='')
+            print(alphabet,end='',flush=True) #flush is imp as without it the compiler can collect whole output and give it at one and animation will not appear 
             time.sleep(0.2)
         print("\n",end='')   #using end=""  because by default it is print("\n", end="\n")
 
@@ -26,7 +26,7 @@ def clear(inp):  #hidden bug like message
 
     print(inp)
 
-    time.sleep(0.2)
+    time.sleep(0.35)
     sys.stdout.write("\033[F")   # Move to previous line
     sys.stdout.write("\033[2K")  # Clear entire line
     sys.stdout.write("\r")   # Move to next line
@@ -44,9 +44,22 @@ for i in ascciv:
 # print("\n",end='')
 
 
-search(1,0.5)
-clear(text)
-lineanime(["hello sahil","how are you"])
+def dots(text):
+    import time
+    import sys
+    for i in range(20):
+        dots = "."*(i%3)
+        sys.stdout.write(f"\r{text}" + dots + "   ")
+        time.sleep(0.1)
+        sys.stdout.flush()
+        time.sleep(0.2)
+dots("starting")
+
+
+if __name__ == "__main__":
+    search(3,0.2)
+    clear(text)
+    lineanime(["hello sahil","how are you"])
 
 #make a game which use random and file handeling it will read file containing word list using random it will replace some character with _ , user will fill the correct value , set lim like each work can use _ max 2 or 3 time # for difficult 2 easy 
 #make a repo which in which a file will take input and convert it into ascci list 
